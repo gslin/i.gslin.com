7ee06df984d1cc54787ca4165f830e9529131ea4
Gea-Suan Lin <gslin@gslin.org>
2023-04-24 17:30:16 +0800

    Add /upload.

--- public/index.php
+++ public/index.php
@@ -6,4 +6,11 @@ call_user_func(function(){
     if ($_SESSION['valid'] !== 1) {
         session_regenerate_id();
     }
+
+    # A simple routing for "/upload".
+    if ($_SERVER['REQUEST_METHOD'] === 'GET' and $_SERVER['REQUEST_URI'] === '/upload') {
+        header('Content-Type: text/html');
+        include(__DIR__ . '/templates/upload.phtml');
+        return;
+    }
 });
